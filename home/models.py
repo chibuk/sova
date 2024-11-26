a4d0e846079cd0059f669df5c7dcd3a14cbb7f62
Юрий <Jouri@localhost.localdomain>
2024-11-26 16:51:55 +0500

    Start point here.

---
+++ home/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from wagtail.models import Page
+# from wagtail.
+
+
+class HomePage(Page):
+    pass
---
+++ sova/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-w$l^g(!s#yq77ex9liinc)qeu^=y#g58-n3thd@l^*^o#$5x39"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
