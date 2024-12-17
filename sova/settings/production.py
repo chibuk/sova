0b5c3e287ce97a019667d5bd46052f4eb11f6228
Jouri Chibuk <chibook@mail.ru>
2024-12-17 19:28:25 +0500

    -1-

--- sova/settings/production.py
+++ sova/settings/production.py
@@ -2,21 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["sport-nur.ru", "www.sport-nur.ru", "185.177.219.173"]
-
-DATABASES = {
-    "default": {
-	"ENGINE": "django.db.backends.postgresql",
-	"NAME": "sova",
-	"USER": "sovauser",
-	"PASSWORD": "GoldGate276",
-	"HOST": "127.0.0.1",
-	"PORT": "",
-    }
-}
-
-
-#try:
-#    from .local import *
-#except ImportError:
-#    pass
+try:
+   from .local import *
+except ImportError:
+   pass
