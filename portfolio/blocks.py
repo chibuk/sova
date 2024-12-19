149a832f88133805b6a68b57e1e8ea467ecf099e
Jouri Chibuk <chibook@mail.ru>
2024-12-19 17:24:19 +0500

    мелкие перемены

--- portfolio/blocks.py
+++ portfolio/blocks.py
@@ -11,25 +11,30 @@ from base.blocks import BaseStreamBlock
 
 
 class CardBlock(StructBlock):
-    heading = CharBlock()
-    text = RichTextBlock(features=['bold', 'italic', 'link'])
-    image = ImageBlock(required=False)
+    heading = CharBlock(label='Заголовок')
+    text = RichTextBlock(features=['bold', 'italic', 'link'], label='Содержание')
+    image = ImageBlock(required=False, label='Изображение')
     
     class Meta:
         icon = "form"
         template = "portfolio/blocks/card_block.html"
+        label='Карточка'
         
         
 class FeaturePostsBlock(StructBlock):
-    heading = CharBlock()
-    text = RichTextBlock(features=["bold", "italic", "link"], required=False)
-    posts = ListBlock(PageChooserBlock(page_type="blog.BlogPage"))
+    heading = CharBlock(label='Заголовок')
+    text = RichTextBlock(features=["bold", "italic", "link"], required=False, label='Содержание')
+    posts = ListBlock(PageChooserBlock(page_type="blog.BlogPage"), label='Записи блога')
     
     class Meta:
         icon = "folder-open-inverse"
         template = "portfolio/blocks/featured_posts_block.html"
+        label='Записи из блога'
 
 
 class PortfolioStreamBlock(BaseStreamBlock):
     card = CardBlock(group="Блоки портфолио")
     featured_posts = FeaturePostsBlock(group="Блоки портфолио")
+
+    class Meta:
+        label='Содержание'
