e5603d47ac420fdb1ccafa9948e04156516a90ce
Юрий <Jouri@localhost.localdomain>
2025-01-16 16:52:19 +0500

    Развитие структуры Учреждения(Institution)

--- institution/models.py
+++ institution/models.py
@@ -4,12 +4,17 @@
 from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
 from modelcluster.fields import ParentalKey
 
 
 class InstitutionPage(Page):
+    '''
+    Учреждение, в данной модели есть Тренеры, Объекты и другие дочерние модели
+    Они и их индексные страницы являются непосрественными потомками этой модели
+    Наслдеование плоское, одного уровня для удобства их связей ParentKey
+    '''
     name = models.CharField('Наименование', max_length=512)
     logo = models.ForeignKey('wagtailimages.Image', null=True, blank=True, verbose_name="Логотип",
                                    on_delete=models.SET_NULL, related_name='+', help_text='Логотип')
@@ -20,7 +25,7 @@ class InstitutionPage(Page):
         index.SearchField('name'),
         index.SearchField('description'),
     ]
-    subpage_types = ['institution.TrainerIndexPage']
+    subpage_types = ['institution.TrainerIndexPage', 'institution.TrainerPage']
     parent_page_types = ['institution.InstitutionIndexPage']
     
     content_panels = Page.content_panels + [
@@ -28,11 +33,12 @@ class InstitutionPage(Page):
         FieldPanel('logo'),
         FieldPanel('description'),
         FieldPanel('site'),
+        InlinePanel('trainer'),
     ]
     
     def get_context(self, request):
         context = super().get_context(request)
-        pages = self.get_children().live()
+        pages = self.get_children().live().not_type(TrainerPage) # в not_type добавляем всех неиндексных детей
         context['indexpages'] = pages
         return context
 
@@ -65,15 +71,21 @@ class TrainerPage(Page):
     education = models.CharField('Образование', max_length=128, null=True, blank=True)
     ranks = models.CharField('Звания и достижения', max_length=128, null=True, blank=True)
     # category = тренерская катергория со сроком действия
+    institution = ParentalKey(
+        'institution.InstitutionPage', 
+        on_delete=models.PROTECT, 
+        related_name='trainer', 
+        verbose_name='Организация',) # default=)
 
     search_fields = Page.search_fields + [
         index.SearchField('name'),
         index.SearchField('surname'),
     ]
     subpage_types = []
-    parent_page_types = ['institution.TrainerIndexPage']
+    parent_page_types = ['institution.InstitutionPage']
     
     content_panels = Page.content_panels + [
+        # FieldPanel('institution'),
         FieldPanel('surname'),
         FieldPanel('name'),
         FieldPanel('name2'),
@@ -83,17 +95,28 @@ class TrainerPage(Page):
         FieldPanel('ranks'),
     ]
 
+    def save(self, *args, **kwargs):
+        # Здесь указываем логику для нахождения родительской страницы
+        parent_page_id = self.get_institution_instance()
+        self.institution = parent_page_id
+        super().save(*args, **kwargs)
+
+    def get_institution_instance(self):
+        parent = self.get_parent()
+        instance = parent.specific_class.objects.get(id=parent.id)
+        return instance
+
 
 class TrainerIndexPage(Page):
     body = RichTextField(blank=True)
     
     def get_context(self, request):
         context = super().get_context(request)
-        pages = self.get_children().live().order_by('trainerpage__name')
+        pages = TrainerPage.objects.live().filter(institution=self.get_parent()).order_by('name')
         context['trainerpages'] = pages
         return context
     
-    subpage_types = ['institution.TrainerPage']
+    subpage_types = []
     parent_page_types = ['institution.InstitutionPage']
     
     content_panels = Page.content_panels + [
