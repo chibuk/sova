d1a0fd59111df95b5e5586e8ecbcee134ddb5458
Юрий <Jouri@localhost.localdomain>
2025-01-31 18:28:24 +0500

    +Кнопка в боковом меню и оформление, фуиер.

--- sova/static/js/sova.js
+++ sova/static/js/sova.js
@@ -3,13 +3,12 @@ document.querySelector('#cover').addEventListener('click', function(event) {
     document.querySelector('#nav-toggle').checked = false;
 });
 
-
-window.addEventListener('resize', function() {
+function modify_menu() {
     /**
-     * Убирает часть пунктов из меню горизонтального в боковую панель,
+     * Перемещает часть пунктов из меню горизонтального в боковую панель,
      * которая открывается по клику по "бургеру" (три горизонтальные полоски)
      * Вид меню: Элемент1 Элемент2 ... ЭлементN Поиск Бургер
-     * @param {*} menu_len число пунктов, оставляемых в горизонтальном основном меню, остальные уйдут в боковую панель
+     * @param {int} menu_len число пунктов, оставляемых в горизонтальном основном меню, остальные уйдут в боковую панель
      */
     function setmenu_panel (menu_len) {
         const nav_ul_element = document.querySelector('nav > ul.menu'); // основное горизонтальное меню
@@ -25,6 +24,11 @@ window.addEventListener('resize', function() {
             }
         }
     };
+    /**
+     * Перемещает часть пунктов из боковой панели в меню горизонтальное.
+     * @param {int} menu_len число пунктов, оставляемых в горизонтальном основном меню,
+     * если там меньше, то добираем, перемещая из боковой панели
+     */
     function setmenu_nav (menu_len) {
         const nav_ul_element = document.querySelector('nav > ul.menu'); // основное горизонтальное меню
         const panel_ul_element = document.querySelector('nav.nav > ul'); // боковая панель меню
@@ -41,13 +45,16 @@ window.addEventListener('resize', function() {
         const header_width = document.querySelector('header').clientWidth;
         if (header_width <= 360) return 0; // mobile
         if (header_width <= 375) return 1; // xmobile
-        if (header_width <= 512) return 2; // xmobile
-        if (header_width <= 768) return 3; // xmobile
-        if (header_width <= 1024) return 4;// tablet
+        if (header_width <= 512) return 2; // xLmobile
+        if (header_width <= 768) return 3; // xxLmobile
+        if (header_width <= 1024) return 4;// tablet и брокк широкие экраны
         return 5;
     }
     const l = _menu_len();
     setmenu_panel(l);
     setmenu_nav(l);
-});
+}; 
+modify_menu(); // отработать при загрузке
+
+window.addEventListener('resize', modify_menu);
 
