32af919527ca57336b58245733da6715035a8b02
Юрий <Jouri@localhost.localdomain>
2025-04-21 17:58:37 +0500

    Работа над дизайном, отображение на LocationPge, EventPage, LocationIndexPage. Ещё не деплоить, завершить!

--- sova/settings/dev.py
+++ sova/settings/dev.py
@@ -3,12 +3,24 @@ from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#    }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sova',
+        'USER': 'sova_user',
+        'PASSWORD': 'GoldGate276',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        },
     }
-}
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
